56085234ed18cb98fcd8799af6650e61ee48c855
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-02-24 12:23:17 +0300

    Create logics.svelte

---
+++ svelte/logics.svelte
@@ -0,0 +1,32 @@
+// {#if condition} js code blok {/if}
+
+<script>
+	let user = { loggedIn: false };
+	function toggle() =>user.loggedIn = !user.loggedIn;
+  let x = 7;
+</script>
+// Classical if block
+{#if user.loggedIn}
+	<button on:click={toggle}>Log out</button>
+{/if}
+
+{#if !user.loggedIn}
+	<button on:click={toggle}>Log in</button>
+{/if}
+
+// if-else block
+{#if user.loggedIn}
+	<button on:click={toggle}>Log out</button>
+{:else}
+	<button on:click={toggle}>Log in</button>
+{/if}
+
+// multi else-if block
+{#if x > 10}
+	<p>{x} is greater than 10</p>
+{:else if 5 > x}
+	<p>{x} is less than 5</p>
+{:else}
+	<p>{x} is between 5 and 10</p>
+{/if}
+
