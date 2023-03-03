f4358a702ed1434739ee6585d7d1fdb2843e2fd8
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-03-03 12:03:12 +0300

    Update Todos.svelte

--- svelte/ToDo App/src/lib/Todos.svelte
+++ svelte/ToDo App/src/lib/Todos.svelte
@@ -1,7 +1,19 @@
+<script>
+  export let todos = [];
+  $: totalTodos = todos.length;
+  $: completedTodos = todos.filter((todo) => todo.completed).length;
+
+  function removeTodo(todo) {
+    todos = todos.filter((e) => e.id !== todo.id);
+  }
+
+  let newTodoName = "";
+</script>
+
 <!-- Todos.svelte -->
 <div class="todoapp stack-large">
   <!-- NewTodo -->
-  
+
   <form>
     <h2 class="label-wrapper">
       <label for="todo-0" class="label__lg"> What needs to be done? </label>
@@ -32,83 +44,38 @@
   </div>
 
   <!-- TodosStatus -->
-  <h2 id="list-heading">2 out of 3 items completed</h2>
+  <h2 id="list-heading">
+    {completedTodos} out of {totalTodos} items completed
+  </h2>
 
   <!-- Todos -->
   <ul role="list" class="todo-list stack-large" aria-labelledby="list-heading">
-    <!-- todo-1 (editing mode) -->
-    <li class="todo">
-      <div class="stack-small">
-        <form class="stack-small">
-          <div class="form-group">
-            <label for="todo-1" class="todo-label">
-              New name for 'Create a Svelte starter app'
-            </label>
-            <input
-              type="text"
-              id="todo-1"
-              autocomplete="off"
-              class="todo-text" />
-          </div>
-          <div class="btn-group">
-            <button class="btn todo-cancel" type="button">
-              Cancel
-              <span class="visually-hidden">renaming Create a Svelte starter app</span>
-            </button>
-            <button class="btn btn__primary todo-edit" type="submit">
-              Save
-              <span class="visually-hidden">new name for Create a Svelte starter app</span>
-            </button>
-          </div>
-        </form>
-      </div>
-    </li>
-
-    <!-- todo-2 -->
-    <li class="todo">
-      <div class="stack-small">
+    {#each todos as todo (todo.id)}
+      <li class="todo">
         <div class="c-cb">
-          <input type="checkbox" id="todo-2" checked />
-          <label for="todo-2" class="todo-label">
-            Create your first component
+          <input
+            type="checkbox"
+            id="todo-{todo.id}"
+            on:click={() => (todo.completed = !todo.completed)}
+            checked={todo.completed}
+          />
+          <label for="todo-{todo.id}" class="todo-label">
+            {todo.name}
           </label>
         </div>
         <div class="btn-group">
           <button type="button" class="btn">
-            Edit
-            <span class="visually-hidden">Create your first component</span>
+            Edit <span class="visually-hidden">{todo.name}</span>
           </button>
-          <button type="button" class="btn btn__danger">
-            Delete
-            <span class="visually-hidden">Create your first component</span>
-          </button>
-        </div>
-      </div>
-    </li>
-
-    <!-- todo-3 -->
-    <li class="todo">
-      <div class="stack-small">
-        <div class="c-cb">
-          <input type="checkbox" id="todo-3" />
-          <label for="todo-3" class="todo-label">
-            Complete the rest of the tutorial
-          </label>
-        </div>
-        <div class="btn-group">
-          <button type="button" class="btn">
-            Edit
-            <span class="visually-hidden">Complete the rest of the tutorial</span>
-          </button>
-          <button type="button" class="btn btn__danger">
-            Delete
-            <span class="visually-hidden">Complete the rest of the tutorial</span>
+          <button class="btn btn__danger" on:click={() => removeTodo(todo)}
+            >Delete
+            <span class="visually-hidden">{todo.name}</span>
           </button>
         </div>
-      </div>
-    </li>
+      </li>
+    {:else}<li>Nothing to do here</li>
+    {/each}
   </ul>
-
   <hr />
 
   <!-- MoreActions -->
@@ -121,5 +88,5 @@
 <br />
 <hr />
 <!-- for disable error message press "ctr + ." and enter -->
-  <!-- svelte-ignore a11y-missing-attribute -->
-  <img height="32" width="88" src="https://www.w3.org/WAI/wcag2A" />
+<!-- svelte-ignore a11y-missing-attribute -->
+<img height="32" width="88" src="https://www.w3.org/WAI/wcag2A" />
