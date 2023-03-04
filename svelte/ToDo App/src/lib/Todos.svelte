e907ff4ee209abb6b920f6d8482a35cca1dc34fc
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-03-04 12:10:56 +0300

    Update Todos.svelte

--- svelte/ToDo App/src/lib/Todos.svelte
+++ svelte/ToDo App/src/lib/Todos.svelte
@@ -1,47 +1,59 @@
 <script>
+  import FilterButton from "./FilterButton.svelte";
+  import Todo from "./Todo.svelte";
+
   export let todos = [];
+
+  let newTodoName = "";
+  $: newTodoId = totalTodos ? Math.max(...todos.map((t) => t.id)) + 1 : 1;
+
   $: totalTodos = todos.length;
   $: completedTodos = todos.filter((todo) => todo.completed).length;
 
   function removeTodo(todo) {
-    todos = todos.filter((e) => e.id !== todo.id);
+    todos = todos.filter((t) => t.id !== todo.id);
   }
 
-  let newTodoName = "";
+  function addTodo() {
+    todos = [...todos, { id: newTodoId, name: newTodoName, completed: false }];
+    newTodoName = "";
+  }
+
+  function updateTodo(todo) {
+    const i = todos.findIndex((t) => t.id === todo.id);
+    todos[i] = { ...todos[i], ...todo };
+  }
+
+  let filter = "all";
+  const filterTodos = (filter, todos) =>
+    filter === "active"
+      ? todos.filter((t) => !t.completed)
+      : filter === "completed"
+      ? todos.filter((t) => t.completed)
+      : todos;
 </script>
 
 <!-- Todos.svelte -->
 <div class="todoapp stack-large">
   <!-- NewTodo -->
-
-  <form>
+  <form on:submit|preventDefault={addTodo}>
     <h2 class="label-wrapper">
       <label for="todo-0" class="label__lg"> What needs to be done? </label>
     </h2>
-    <input type="text" id="todo-0" autocomplete="off" class="input input__lg" />
+    <input
+      bind:value={newTodoName}
+      type="text"
+      id="todo-0"
+      autocomplete="off"
+      class="input input__lg"
+    />
     <button type="submit" disabled="" class="btn btn__primary btn__lg">
       Add
     </button>
   </form>
 
   <!-- Filter -->
-  <div class="filters btn-group stack-exception">
-    <button class="btn toggle-btn" aria-pressed="true">
-      <span class="visually-hidden">Show</span>
-      <span>All</span>
-      <span class="visually-hidden">tasks</span>
-    </button>
-    <button class="btn toggle-btn" aria-pressed="false">
-      <span class="visually-hidden">Show</span>
-      <span>Active</span>
-      <span class="visually-hidden">tasks</span>
-    </button>
-    <button class="btn toggle-btn" aria-pressed="false">
-      <span class="visually-hidden">Show</span>
-      <span>Completed</span>
-      <span class="visually-hidden">tasks</span>
-    </button>
-  </div>
+  <FilterButton bind:filter />
 
   <!-- TodosStatus -->
   <h2 id="list-heading">
@@ -50,32 +62,19 @@
 
   <!-- Todos -->
   <ul role="list" class="todo-list stack-large" aria-labelledby="list-heading">
-    {#each todos as todo (todo.id)}
+    {#each filterTodos(filter, todos) as todo (todo.id)}
       <li class="todo">
-        <div class="c-cb">
-          <input
-            type="checkbox"
-            id="todo-{todo.id}"
-            on:click={() => (todo.completed = !todo.completed)}
-            checked={todo.completed}
-          />
-          <label for="todo-{todo.id}" class="todo-label">
-            {todo.name}
-          </label>
-        </div>
-        <div class="btn-group">
-          <button type="button" class="btn">
-            Edit <span class="visually-hidden">{todo.name}</span>
-          </button>
-          <button class="btn btn__danger" on:click={() => removeTodo(todo)}
-            >Delete
-            <span class="visually-hidden">{todo.name}</span>
-          </button>
-        </div>
+        <Todo
+          {todo}
+          on:update={(e) => updateTodo(e.detail)}
+          on:remove={(e) => removeTodo(e.detail)}
+        />
       </li>
-    {:else}<li>Nothing to do here</li>
+    {:else}
+      <li>Nothing to do here!</li>
     {/each}
   </ul>
+
   <hr />
 
   <!-- MoreActions -->
@@ -84,9 +83,3 @@
     <button type="button" class="btn btn__primary">Remove completed</button>
   </div>
 </div>
-
-<br />
-<hr />
-<!-- for disable error message press "ctr + ." and enter -->
-<!-- svelte-ignore a11y-missing-attribute -->
-<img height="32" width="88" src="https://www.w3.org/WAI/wcag2A" />
