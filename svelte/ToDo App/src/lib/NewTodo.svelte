a72249d28704cb4626c7a105ca43eac3e221abd5
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-03-07 12:17:36 +0300

    Update NewTodo.svelte

--- svelte/ToDo App/src/lib/NewTodo.svelte
+++ svelte/ToDo App/src/lib/NewTodo.svelte
@@ -1,44 +1,102 @@
 <script>
   import { createEventDispatcher } from "svelte";
-  import { onMount } from "svelte";
-
   const dispatch = createEventDispatcher();
 
-  export let autofocus = false;
+  import { selectOnFocus } from "../actions.js";
 
-  let name = "";
-  let nameEl;
+  export let todo;
 
-  const addTodo = () => {
-    dispatch("addTodo", name);
-    name = "";
-    nameEl.focus();
-  };
-  const onCancel = () => {
-    name = "";
-    nameEl.focus();
-  };
+  let editing = false;
+  let editButtonPressed = false;
+  let name = todo.name;
 
-  onMount(() => autofocus && nameEl.focus());
+  function update(updatedTodo) {
+    todo = { ...todo, ...updatedTodo };
+    dispatch("update", todo);
+  }
+
+  function onCancel() {
+    name = todo.name;
+    editing = false;
+  }
+
+  function onSave() {
+    update({ name: name });
+    editing = false;
+  }
+
+  function onRemove() {
+    dispatch("remove", todo);
+  }
+
+  function onEdit() {
+    editButtonPressed = true;
+    editing = true;
+  }
+
+  function onToggle() {
+    update({ completed: !todo.completed });
+  }
+
+  const focusOnInit = (node) =>
+    node && typeof node.focus === "function" && node.focus();
+
+  const focusEditButton = (node) => editButtonPressed && node.focus();
 </script>
 
-<form
-  on:submit|preventDefault={addTodo}
-  on:keydown={(e) => e.key === "Escape" && onCancel()}
->
-  <h2 class="label-wrapper">
-    <label for="todo-0" class="label__lg">What needs to be done?</label>
-  </h2>
-  <input
-    bind:value={name}
-    bind:this={nameEl}
-    type="text"
-    id="todo-0"
-    autocomplete="off"
-    class="input input__lg"
-  />
-
-  <button type="submit" disabled={!name} class="btn btn__primary btn__lg"
-    >Add</button
-  >
-</form>
+<div class="stack-small">
+  {#if editing}
+    <!-- markup for editing todo: label, input text, Cancel and Save Button -->
+    <form
+      on:submit|preventDefault={onSave}
+      class="stack-small"
+      on:keydown={(e) => e.key === "Escape" && onCancel()}
+    >
+      <div class="form-group">
+        <label for="todo-{todo.id}" class="todo-label"
+          >New name for '{todo.name}'</label
+        >
+        <input
+          bind:value={name}
+          use:selectOnFocus
+          use:focusOnInit
+          type="text"
+          id="todo-{todo.id}"
+          autoComplete="off"
+          class="todo-text"
+        />
+      </div>
+      <div class="btn-group">
+        <button class="btn todo-cancel" on:click={onCancel} type="button">
+          Cancel<span class="visually-hidden">renaming {todo.name}</span>
+        </button>
+        <button
+          class="btn btn__primary todo-edit"
+          type="submit"
+          disabled={!name}
+        >
+          Save<span class="visually-hidden">new name for {todo.name}</span>
+        </button>
+      </div>
+    </form>
+  {:else}
+    <!-- markup for displaying todo: checkbox, label, Edit and Delete Button -->
+    <div class="c-cb">
+      <input
+        type="checkbox"
+        id="todo-{todo.id}"
+        on:click={onToggle}
+        checked={todo.completed}
+      />
+      <label for="todo-{todo.id}" class="todo-label">{todo.name}</label>
+    </div>
+    <div class="btn-group">
+      <button type="button" class="btn" on:click={onEdit} use:focusEditButton>
+        Edit<span class="visually-hidden"> {todo.name}</span>
+      </button>
+      <button type="button" class="btn btn__danger" on:click={onRemove}>
+        Delete<span class="visually-hidden"> {todo.name}</span>
+      </button>
+    </div>
+  {/if}
+</div>
