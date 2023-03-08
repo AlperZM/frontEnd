d59bbd4f9f146f098e1f85a2bd0d5fb335f94b60
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-03-08 12:27:30 +0300

    Update Todos.svelte

--- svelte/ToDo App/src/lib/Todos.svelte
+++ svelte/ToDo App/src/lib/Todos.svelte
@@ -4,6 +4,7 @@
   import MoreActions from "./MoreActions.svelte";
   import NewTodo from "./NewTodo.svelte";
   import TodosStatus from "./TodosStatus.svelte";
+  import {alert} from "../stores.js";
 
   export let todos = [];
 
@@ -14,18 +15,34 @@
   function removeTodo(todo) {
     todos = todos.filter((t) => t.id !== todo.id);
     todosStatus.focus();
+    $alert = `Todo '${todo.name}' has been deleted`;
   }
 
   function addTodo(name) {
     todos = [...todos, { id: newTodoId, name, completed: false }];
+    $alert = `ToDo ${name} has benn addded`;
   }
 
-  function updateTodo(todo) {
-    const i = todos.findIndex((t) => t.id === todo.id);
-    todos[i] = { ...todos[i], ...todo };
-  }
+function updateTodo(todo) {
+  const i = todos.findIndex((t) => t.id === todo.id)
+  if (todos[i].name !== todo.name)            $alert = `todo '${todos[i].name}' has been renamed to '${todo.name}'`
+  if (todos[i].completed !== todo.completed)  $alert = `todo '${todos[i].name}' marked as ${todo.completed ? 'completed' : 'active'}`
+  todos[i] = { ...todos[i], ...todo }
+}
+
 
   let filter = "all";
+$: {
+  if (filter === 'all') {
+    $alert = 'Browsing all to-dos';
+  } else if (filter === 'active') {
+    $alert = 'Browsing active to-dos';
+  } else if (filter === 'completed') {
+    $alert = 'Browsing completed to-dos';
+  }
+}
+
+  
   const filterTodos = (filter, todos) =>
     filter === "active"
       ? todos.filter((t) => !t.completed)
@@ -33,12 +50,15 @@
       ? todos.filter((t) => t.completed)
       : todos;
 
-  const checkAllTodos = (completed) => {
-    todos = todos.map((t) => ({ ...t, completed }));
-  };
+const checkAllTodos = (completed) => {
+  todos = todos.map((t) => ({...t, completed}))
+  $alert = `${completed ? 'Checked' : 'Unchecked'} ${todos.length} to-dos`
+}
 
-  const removeCompletedTodos = () =>
-    (todos = todos.filter((t) => !t.completed));
+const removeCompletedTodos = () => {
+  $alert = `Removed ${todos.filter((t) => t.completed).length} to-dos`
+  todos = todos.filter((t) => !t.completed)
+}
 </script>
 
 <div class="todoapp stack-large">
