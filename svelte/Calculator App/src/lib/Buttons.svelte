a39bb8252503e8782f61eec77ecae8fe86debf6d
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-03-13 14:21:45 +0300

    Create

---
+++ svelte/Calculator App/src/lib/Buttons.svelte
@@ -0,0 +1,22 @@
+<script>
+//  export let display;
+  export let buttonId;
+  let num1 = "0";
+  let num2 = "0";
+  let operant;
+  function numbers(){
+   return num1 += buttonId;
+  }
+</script>
+<button type="button" on:click={numbers}>
+  {buttonId}
+</button>
+
+<style>
+  button{
+    display: absolute;
+    width: 40px;
+    height: 40px;
+    margin: 5px;
+  }
+</style>
