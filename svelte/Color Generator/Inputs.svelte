563e543403f34083739af3a8ba638611833cae61
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-05-12 13:02:25 +0300

    Create Inputs.svelte
    create input components for color generator app

---
+++ svelte/Color Generator/Inputs.svelte
@@ -0,0 +1,42 @@
+<script>
+  let numbers = 0;
+  export let cols = "";
+</script>
+
+<label
+  ><span class="title">{cols}:</span>
+  <input
+    name={cols}
+    type="number"
+    min="0"
+    max="255"
+    bind:value={numbers}
+    step="1"
+    on:change
+  />
+  <input
+    name={cols}
+    type="range"
+    min="0"
+    max="255"
+    bind:value={numbers}
+    step="1"
+    on:change
+  />
+</label>
+
+<style>
+  * {
+    font-family: Arial, sans-serif;
+  }
+  label {
+    display: flex;
+  }
+  input,
+  span {
+    margin: 10px;
+  }
+  .title {
+    font-size: 18px;
+  }
+</style>
