2fd1e5a9f320811601e488d1ce8f5a6090957486
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-06-16 10:36:43 +0300

    Create Footer.svelte

---
+++ Svelte Projects/src/lib/Footer.svelte
@@ -0,0 +1,28 @@
+<script></script>
+
+<footer id="footer">
+  <div class="box">
+    <p>Made with Svelte by Seges</p>
+  </div>
+  <div class="box">
+    <a href="#">Terms</a>
+    <a href="#">Contact</a>
+  </div>
+  <div class="box">Follow us:</div>
+</footer>
+
+<style>
+  #footer {
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+    width: auto;
+    height: 80px;
+    margin-left: 120px;
+    background: var(--secondary);
+  }
+  .box {
+    width: 200px;
+    height: auto;
+  }
+</style>
